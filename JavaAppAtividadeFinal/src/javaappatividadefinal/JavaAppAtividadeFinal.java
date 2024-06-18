7bd4fd74c8327ce230cbc3667b7082ea343c3c31
MateusZavagli <131975867+MateusZavagli@users.noreply.github.com>
2024-06-18 19:35:37 -0300

    Add files via upload

---
+++ JavaAppAtividadeFinal/src/javaappatividadefinal/JavaAppAtividadeFinal.java
@@ -0,0 +1,16 @@
+package javaappatividadefinal;
+
+import interfaces.TelaLogin;
+
+/**
+ * @author mateus.zlcarmo
+ */
+public class JavaAppAtividadeFinal {
+
+    public static void main(String[] args) {
+      TelaLogin l = new TelaLogin();
+      l.setVisible(true);
+    }
+}
+    
+
